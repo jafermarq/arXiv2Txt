907426a1f97cf58ed05cabf624c9c31c68d99ba1
jafermarq <jafermarq@gmail.com>
2018-01-28 11:16:04 +0000

    minor fixes

--- rename.sh
+++ rename.sh
@@ -3,10 +3,10 @@
 NEW_NAME=`python <<END
 import arxiv
 import ntpath
-fileName = "$1"
+fileName = ntpath.basename("$1")
 arxiv_id = fileName[:fileName.rfind('.')]
 title = arxiv.query(id_list=[arxiv_id])[0]['title_detail']['value']
 print(title + ".pdf")
 END`
 
-mv $1 "${NEW_NAME}"
+mv "$1" "${NEW_NAME}"
